571de10f7420dfefca95ff03d0c8faaf9723d730
Yoong111 <81151290+Yoong111@users.noreply.github.com>
2023-05-26 14:15:39 +0800

    Update EngagingCaoCao.java

--- Code/EngagingCaoCao.java
+++ Code/EngagingCaoCao.java
@@ -2,123 +2,153 @@ package assignment_ds;
 
 import java.util.*;
 
-public class FoodHarvesting {
+public class EngagingCaoCao {
 
-    private Map<Integer, List<Integer>> adjacencyList;
-    private boolean[] visited;
-    private List<Integer> path;
-    private int startNode;
+    private static final int[][] MAZE = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+        {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
 
-    public FoodHarvesting() {
-        // Initialize the instance variables with default values
-        adjacencyList = new HashMap<>();
-        startNode = 1;
-        visited = new boolean[10 + 1]; // Add 1 for 0-based indexing
-        path = new ArrayList<>();
+    private static final int ROWS = MAZE.length;
+    private static final int COLS = MAZE[0].length;
+    private static final int START = 2;
+    private static final int EXIT = 3;
 
-    }
+    private static List<Integer> path;
+    static Scanner sc = new Scanner(System.in);
 
-    public List<Integer> findPath(int currentNode, List<Integer> currentPath) {
-        visited[currentNode] = true;
+    public void engagingCCSelection() {
+        System.out.println("<<< Engaging CaoCao >>>\n");
+        System.out.println("Finally, Cao Cao lost The Battle of Red Cliff. He retreated away from the river via Hua\n"
+                + "Rong Road and managed to escape. Hua Rong Road is a road with complex terrains.\n"
+                + "Show how Cao Cao might have retreated from Hua Rong Road so that Liu Bei and\n"
+                + "Zhao Yun can catch up with him. Besides, Guan Yu is ahead and is engaging Cao Cao\n"
+                + "at the exit of the maze.");
+        System.out.println("\n2D maze of Hua Rong Road: ");
+        System.out.println("(2 denotes the starting point. 3 denotes the exit of the maze.)");
 
-        List<Integer> neighbors = adjacencyList.get(currentNode);
-        if (neighbors != null) {
-            for (int neighbor : neighbors) {
-                if (!visited[neighbor]) {
-                    currentPath.add(neighbor);
-                    findPath(neighbor, currentPath);
-                    currentPath.remove(currentPath.size() - 1);
-                }
+        for (int row = 0; row < MAZE.length; row++) {
+            for (int col = 0; col < MAZE[row].length; col++) {
+                System.out.print(MAZE[row][col] + " ");
             }
+            System.out.println();
         }
-
-        if (currentPath.size() > path.size()) {
-            path = new ArrayList<>(currentPath);
-        }
-
-        visited[currentNode] = false;
-
-        return path;
-    }
-
-    public void foodHarvestingSelection() {
-        Scanner sc = new Scanner(System.in);
-
-        // Example input: adjacency list and node without food
-        adjacencyList.put(1, Arrays.asList(2, 3, 6, 10));
-        adjacencyList.put(2, Arrays.asList(1, 4));
-        adjacencyList.put(3, Arrays.asList(1, 4, 7));
-        adjacencyList.put(4, Arrays.asList(2, 3, 5));
-        adjacencyList.put(5, Arrays.asList(4, 6, 7));
-        adjacencyList.put(6, Arrays.asList(1, 5, 7, 8));
-        adjacencyList.put(7, Arrays.asList(5, 6, 8, 9));
-        adjacencyList.put(8, Arrays.asList(6, 7, 9, 10));
-        adjacencyList.put(9, Arrays.asList(7, 8, 10));
-        adjacencyList.put(10, Arrays.asList(1, 8, 9));
-
-        System.out.println("<<< Food Harvesting >>>\n");
-        System.out.println("We need to depart from Sun Wu’s camp (Node 1) and harvest all of the food on each\n"
-                + "node and back to Sun Wu’s camp without passing through a node twice.\n"
-                + "This system will show the path.");
         System.out.println("\n--------------------------------------------------------\n");
 
-        int nodeWithoutFood = 0;
-
-        while (true) {
-
-            System.out.print("Enter Node Without Food [-1 to exit]: ");
-
-            try {
-                nodeWithoutFood = sc.nextInt();
-                if (nodeWithoutFood < 2 && nodeWithoutFood != -1 || nodeWithoutFood > 10 && nodeWithoutFood != -1) {
-                    throw new IllegalArgumentException("Invalid input. Allowed input:[2 ~ 10]\n");
-                }
-                break;
-            } catch (InputMismatchException ex) {
-                System.out.println("Invalid input. Input must be an integer.\n");
-                // Clear the invalid input
-                sc.next();
-            } catch (IllegalArgumentException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-        sc.nextLine( );//avoid Scanner skipping line problem
-
-        if (nodeWithoutFood == -1) {
-            return;
-        }
-
-        path = new ArrayList<>();
-        List<Integer> path = findPath(1, new ArrayList<>());
-
-        int[] nodeCannotWithout = {4,8,6,10};
-
-        // Remove the node without food if present
-        boolean canRemove= true;
-        for(int i = 0; i<nodeCannotWithout.length;i++){
-            if (nodeCannotWithout[i]==nodeWithoutFood) {
-                canRemove= false;
-            }
-        }
-        if(canRemove){
-            path.remove(Integer.valueOf(nodeWithoutFood));
-        }
-        else{
-            System.out.println("Path must contain Node "+nodeWithoutFood+" to connect all the food.");
-        }
-
-        // Print the path
-        System.out.println("\nPath:");
-        System.out.print(startNode + " -> ");
-        for (int i = 0; i < path.size(); i++) {
-            System.out.print(path.get(i) + " -> ");
-        }
-        System.out.print(startNode + "\n");
-
-        System.out.println("\n--------------------------------------------------------\n");
-
-        System.out.println("Enter to go back to Main Panel");
+        System.out.println("Enter to find the path: ");
         sc.nextLine();
+
+        path = findPath();
+        displayPath();
+
+    }
+
+    private static List<Integer> findPath() {
+        Queue<Integer> queue = new LinkedList<>();
+        Map<Integer, Integer> parentMap = new HashMap<>();
+        boolean[][] visited = new boolean[ROWS][COLS];
+
+        int startRow = -1, startCol = -1;
+
+        // Find the starting position
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                if (MAZE[row][col] == START) {
+                    startRow = row;
+                    startCol = col;
+                    break;
+                }
+            }
+        }
+
+        if (startRow == -1 || startCol == -1) {
+            return new ArrayList<>(); // No valid starting position found
+        }
+
+        // Perform BFS
+        queue.offer(startRow * COLS + startCol);
+        visited[startRow][startCol] = true;
+
+        while (!queue.isEmpty()) {
+            int currentPosition = queue.poll();
+            int row = currentPosition / COLS;
+            int col = currentPosition % COLS;
+
+            // Check if the current position is the exit
+            if (MAZE[row][col] == EXIT) {
+                // Reconstruct the path from the exit to the starting position
+                List<Integer> path = new ArrayList<>();
+                int current = currentPosition;
+                while (current != startRow * COLS + startCol) {
+                    path.add(current);
+                    current = parentMap.get(current);
+                }
+                path.add(startRow * COLS + startCol);
+                Collections.reverse(path);
+                return path;
+            }
+
+            // Explore adjacent positions
+            int[] dr = {-1, 0, 1, 0};
+            int[] dc = {0, 1, 0, -1};
+            for (int i = 0; i < 4; i++) {
+                int newRow = row + dr[i];
+                int newCol = col + dc[i];
+                int newPosition = newRow * COLS + newCol;
+
+                if (newRow >= 0 && newRow < ROWS && newCol >= 0 && newCol < COLS && MAZE[newRow][newCol] != 1
+                        && !visited[newRow][newCol]) {
+                    queue.offer(newPosition);
+                    visited[newRow][newCol] = true;
+                    parentMap.put(newPosition, currentPosition);
+                }
+            }
+        }
+
+        return new ArrayList<>(); // No path found
+    }
+
+    private static void displayPath() {
+        char[][] pathMatrix = new char[ROWS][COLS];
+
+        // Initialize pathMatrix with the maze
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                if (MAZE[row][col] == 1 || MAZE[row][col] == 0) {
+                    pathMatrix[row][col] = (char) (MAZE[row][col] + '0'); // Convert 1 and 0 to char
+                } else {
+                    pathMatrix[row][col] = ' '; // Leave other numbers as they are
+                }
+            }
+        }
+
+        // Mark the path in pathMatrix
+        for (int position : path) {
+            int row = position / COLS;
+            int col = position % COLS;
+            pathMatrix[row][col] = '.'; // Mark as path with dot
+        }
+
+        // Display the path matrix
+        System.out.println("\nPath of how Cao Cao might have escaped: ");
+        System.out.println("(. represents the path)");
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                System.out.print(pathMatrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n--------------------------------------------------------\n");
+
     }
 
 }
