cd4862248fe0e9a0832ccd5a1e8f3f1c68274875
Yoong111 <81151290+Yoong111@users.noreply.github.com>
2023-05-29 17:56:35 +0800

    Update FoodHarvesting.java

--- Code/FoodHarvesting.java
+++ Code/FoodHarvesting.java
@@ -6,39 +6,47 @@ public class FoodHarvesting {
 
     private Map<Integer, List<Integer>> adjacencyList;
     private boolean[] visited;
-    private List<Integer> path;
-    private int startNode;
+    private List<List<Integer>> longestPaths;
 
     public FoodHarvesting() {
         // Initialize the instance variables with default values
         adjacencyList = new HashMap<>();
-        startNode = 1;
-        visited = new boolean[10 + 1]; // Add 1 for 0-based indexing
-        path = new ArrayList<>();
-
+        visited = new boolean[10 + 1]; // Add 1 for 0-based indexing [0 - 10]
+        longestPaths = new ArrayList<>();
     }
 
-    public List<Integer> findPath(int currentNode, List<Integer> currentPath) {
+    public List<List<Integer>> findPaths(Map<Integer, List<Integer>> adjacencyList_temp, int currentNode, List<Integer> currentPath, int nodeWithoutFood) {
         visited[currentNode] = true;
 
-        List<Integer> neighbors = adjacencyList.get(currentNode);
+        List<Integer> neighbors = adjacencyList_temp.get(currentNode); // = get value = get neighbours list
         if (neighbors != null) {
             for (int neighbor : neighbors) {
-                if (!visited[neighbor]) {
+                if (!visited[neighbor] && neighbor != nodeWithoutFood) {
                     currentPath.add(neighbor);
-                    findPath(neighbor, currentPath);
+                    findPaths(adjacencyList_temp, neighbor, currentPath, nodeWithoutFood);
                     currentPath.remove(currentPath.size() - 1);
                 }
             }
         }
 
-        if (currentPath.size() > path.size()) {
-            path = new ArrayList<>(currentPath);
+        visited[currentNode] = false;
+        
+        // Add possible path to longestPaths
+        if (neighbors != null) {
+            // normal case ~ node without food cant be passed through
+            if (nodeWithoutFood >= 2 && nodeWithoutFood <= 9) {
+                if (currentPath.size() == 8 && neighbors.contains(1)) { // Ensure the last node is connected to 1
+                    longestPaths.add(new ArrayList<>(currentPath));
+                }
+            // special case ~ all nodes includes node without food have to be passed through
+            } else {
+                if (currentPath.size() == 9 && neighbors.contains(1)) { // Ensure the last node is connected to 1
+                    longestPaths.add(new ArrayList<>(currentPath));
+                }
+            }
         }
 
-        visited[currentNode] = false;
-
-        return path;
+        return longestPaths;
     }
 
     public void foodHarvestingSelection() {
@@ -47,7 +55,7 @@ public class FoodHarvesting {
         // Example input: adjacency list and node without food
         adjacencyList.put(1, Arrays.asList(2, 3, 6, 10));
         adjacencyList.put(2, Arrays.asList(1, 4));
-        adjacencyList.put(3, List.of(7));
+        adjacencyList.put(3, Arrays.asList(1, 4, 7));
         adjacencyList.put(4, Arrays.asList(2, 3, 5));
         adjacencyList.put(5, Arrays.asList(4, 6, 7));
         adjacencyList.put(6, Arrays.asList(1, 5, 7, 8));
@@ -70,8 +78,8 @@ public class FoodHarvesting {
 
             try {
                 nodeWithoutFood = sc.nextInt();
-                if (nodeWithoutFood < 2 && nodeWithoutFood != -1 || nodeWithoutFood > 10 && nodeWithoutFood != -1) {
-                    throw new IllegalArgumentException("Invalid input. Allowed input:[2 ~ 10]\n");
+                if ((nodeWithoutFood < 2 || nodeWithoutFood > 10) && nodeWithoutFood != -1) {
+                    throw new IllegalArgumentException("Invalid input. Allowed input:[-1],[2 ~ 10]\n");
                 }
                 break;
             } catch (InputMismatchException ex) {
@@ -82,43 +90,64 @@ public class FoodHarvesting {
                 System.out.println(ex.getMessage());
             }
         }
-        sc.nextLine( );//avoid Scanner skipping line problem
+        sc.nextLine(); // Avoid Scanner skipping line problem
 
         if (nodeWithoutFood == -1) {
             return;
         }
 
-        path = new ArrayList<>();
-        List<Integer> path = findPath(1, new ArrayList<>());
+        // Create a copy of adjacencyList to remain the original adjacencyList which will be used in special case
+        Map<Integer, List<Integer>> adjacencyList_temp = new HashMap<>();
+        for (Map.Entry<Integer, List<Integer>> entry : adjacencyList.entrySet()) {
+            Integer node = entry.getKey();
+            List<Integer> neighbors = entry.getValue();
 
-        int[] nodeCannotWithout = {4,8,6,10};
+            List<Integer> neighborsCopy = new ArrayList<>(neighbors);
+            adjacencyList_temp.put(node, neighborsCopy);
+        }
 
-        // Remove the node without food if present
-        boolean canRemove= true;
-        for(int i = 0; i<nodeCannotWithout.length;i++){
-            if (nodeCannotWithout[i]==nodeWithoutFood) {
-                canRemove= false;
+        // Iterate over the entries of the HashMap of copy of adjacencyList
+        for (Map.Entry<Integer, List<Integer>> entry : adjacencyList_temp.entrySet()) {
+            List<Integer> list = new ArrayList<>(entry.getValue());
+
+            // Check if the last contains the number to remove
+            if (list.contains(nodeWithoutFood)) {
+                // Remove the number from the list
+                list.remove(Integer.valueOf(nodeWithoutFood));
             }
-        }
-        if(canRemove){
-            path.remove(Integer.valueOf(nodeWithoutFood));
-        }
-        else{
-            System.out.println("Path must contain Node "+nodeWithoutFood+" to connect all the food.");
+
+            // Update the value in the HashMap
+            entry.setValue(list);
         }
 
-        // Print the path
-        System.out.println("\nPath:");
-        System.out.print(startNode + " -> ");
-        for (int i = 0; i < path.size(); i++) {
-            System.out.print(path.get(i) + " -> ");
+        longestPaths.clear();
+        
+        // Find all possible paths
+        List<List<Integer>> paths = findPaths(adjacencyList_temp, 1, new ArrayList<>(), nodeWithoutFood);
+
+        // Special case ~ all nodes includes node without food have to be passed through
+        if (paths.size() == 0) {
+            System.out.println("\nPath must pass through Node " + nodeWithoutFood + " to harvest all the food.");
+            // Replace nodeWithoutFood with number other than [1~10], so that no node will be removed
+            // Use the original adjacencyList
+            paths = findPaths(adjacencyList, 1, new ArrayList<>(), 11);
         }
-        System.out.print(startNode + "\n");
+
+        // Add node 1 to the beginning and end of each path
+        for (List<Integer> path : paths) {
+            path.add(0, 1);
+            path.add(1);
+        }
+
+        // Print the paths
+        System.out.println("\nPaths:");
+        for (List<Integer> path : paths) {
+            System.out.println(path);
+        }
 
         System.out.println("\n--------------------------------------------------------\n");
 
         System.out.println("Enter to go back to Main Panel");
         sc.nextLine();
     }
-
 }
